a84448436b097f63abed2d54980731171b549db1
Joshua Green <joshmg@gmail.com>
2011-06-19 16:14:24 -0400

    Updated latch_test_unix.cpp to the new testing paradigm.

--- latch_test_unix.cpp
+++ latch_test_unix.cpp
@@ -7,54 +7,31 @@
 #include <pthread.h>
 #include <cstdlib>
 #include <time.h>
+#include <string>
 using namespace std;
 
 latch my_lock;
 
+const unsigned int iterations = 100;
+volatile unsigned int count = 0;
+volatile unsigned int count_max = 0;
+
 const LATCH_PRIORITIES::PRIORITY PRIORITIES[] = { LATCH_PRIORITIES::LOWEST, LATCH_PRIORITIES::LOW, LATCH_PRIORITIES::MEDIUM, LATCH_PRIORITIES::HIGH, LATCH_PRIORITIES::HIGHEST };
-
-void *latch_test_high_priority(void* p) {
-    for (int i=0;i<100;i++) {
-
-        my_lock.lock(LATCH_PRIORITIES::HIGHEST);
-        cout << "[HIGHEST] Locked." << endl;
-        cout.flush();
-
-        suspend(0);
-
-        my_lock.unlock();
-        cout << "[HIGHEST] UnLocked." << endl;
-        cout.flush();
-    }
-}
-
-void *latch_test_low_priority(void* p) {
-    for (int i=0;i<100;i++) {
-
-        my_lock.lock(LATCH_PRIORITIES::LOWEST);
-        cout << "[LOWEST] Locked." << endl;
-        cout.flush();
-
-        suspend(1000);
-
-        my_lock.unlock();
-        cout << "[LOWEST] UnLocked." << endl;
-        cout.flush();
-    }
-}
+string priority_names[] = { "LOWEST", "LOW", "MEDIUM", "HIGH", "HIGHEST" };
 
 void *latch_test(void* p) {
-    for (int i=0;i<100;i++) {
+    for (unsigned int i=0;i<iterations;i++) {
+        unsigned long long priority = ((unsigned long long)p);
+        if (priority > 4) priority = rand()%5;
 
-        my_lock.lock(PRIORITIES[rand()%5]);
-        cout << "Locked." << endl;
-        cout.flush();
+        my_lock.lock(PRIORITIES[priority]);
+        cout << "[" << priority_names[priority] << "] Locked." << endl;
 
-        suspend(0);
+        ++count;
+        //suspend(0);
 
         my_lock.unlock();
-        cout << "UnLocked." << endl;
-        cout.flush();
+        cout << "[" << priority_names[priority] << "] UnLocked." << endl;
     }
 }
 
@@ -63,24 +40,22 @@ int main() {
     cout << "Created lock." << endl;
     cout.flush();
 
-    pthread_t t1, t2, t3, t4, t5;
+    pthread_t t1, t2, t3, t4, t5, t6;
     pthread_attr_t pthread_custom_attr;
     pthread_attr_init(&pthread_custom_attr);
 
-    pthread_create(&t1, 0, latch_test_low_priority, (void*)0);
-    pthread_create(&t2, 0, latch_test_high_priority, (void*)0);
-    pthread_create(&t3, 0, latch_test_low_priority,  (void*)0);
-    pthread_create(&t4, 0, latch_test_high_priority, (void*)0);
-    pthread_create(&t5, 0, latch_test_high_priority, (void*)0);
+    pthread_create(&t1, 0, latch_test, (void*)5);    ++count_max;
+    pthread_create(&t2, 0, latch_test, (void*)3);    ++count_max;
+    pthread_create(&t3, 0, latch_test, (void*)4);    ++count_max;
+    pthread_create(&t4, 0, latch_test, (void*)1);    ++count_max;
+    pthread_create(&t5, 0, latch_test, (void*)2);    ++count_max;
+    pthread_create(&t6, 0, latch_test, (void*)0);    ++count_max;
 
-    pthread_join(t1, 0);
-    pthread_join(t2, 0);
-    pthread_join(t3, 0);
-    pthread_join(t4, 0);
-    pthread_join(t5, 0);
-
-    char c;
-    cin >> c;
+    while (count < count_max*iterations) {
+        cout << (count/((float)count_max*iterations))*100.0 << "%" << endl;
+        suspend(1000);
+    }
+    cout << "Done." << endl;
 
     return 0;
 }
